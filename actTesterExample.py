8c817386cdeb4281d23a767e00c26bbb798adaa7
Chris May <chris.ray.may@gmail.com>
2019-03-22 15:53:02 -0400

    Commented out functions
    Commented out functions that aren't being used, the non-corrected syntax in them was causing errors when running

--- actTesterExample.py
+++ actTesterExample.py
@@ -29,66 +29,66 @@ isStart = [] 'A list of the epoch times where each pin transitioned from HIGH to
 availableStations = 3 - t1Samp - t2Samp 'Number of available test stations is 3 - the stations assigned to other tests' 
 stStatus = [] 'Show the status of each relay as available/taken'
 '''
-def switchCheck(pins, delay):
-    '''Because we'll likely be using mechanical switches in the actuators we're testing, we need to de-bounce the switches before
-    counting a cycle input. We're using low active for the inputs (tied to GND)
-    This function tracks the state of the input during the last call and compares it against the current state of the input
-'''
-    #for pins in inputStates:
-    #    if inputStates[pins] == LOW & isLast[pins] == HIGH: 'If the input is currently LOW and the last state was HIGH, the function logs the current epoch time as START'
-    #    isStart[pins] = time.time()
-    #    inputStates[pins] = LOW
-    #    elif inputStates[pins] == LOW & isLast[pins] == LOW: 'If the current state is LOW and the last state was LOW, compare the START time against the debounce delay'
-    #        if time.time() - isStart[pins] > delay: '- If the difference between the current time and the start time is greater than the delay, increment the cycle counter'
-    #        test1 += 1
-    #        else:
-    #        'do nothing' '- If the difference is less than the delay, do nothing'
-    #    elif inputStates[pins] == HIGH & isLast[pins] == LOW: 'If the current state is HIGH and the last state was LOW, reset the last state to HIGH'
-    #    isLast[pins] = HIGH 
-    #    elif inputStates[pins] == HIGH & isLAst[pins] == HIGH: 'If the current state is HIGH and the last state was HIGH, do nothing'
-    #    'do nothing'
+#def switchCheck(pins, delay):
+#    '''Because we'll likely be using mechanical switches in the actuators we're testing, we need to de-bounce the switches before
+#    counting a cycle input. We're using low active for the inputs (tied to GND)
+#    This function tracks the state of the input during the last call and compares it against the current state of the input
+#'''
+#    for pins in inputStates:
+#        if inputStates[pins] == LOW & isLast[pins] == HIGH: 'If the input is currently LOW and the last state was HIGH, the function logs the current epoch time as START'
+#        isStart[pins] = time.time()
+#        inputStates[pins] = LOW
+#        elif inputStates[pins] == LOW & isLast[pins] == LOW: 'If the current state is LOW and the last state was LOW, compare the START time against the debounce delay'
+#            if time.time() - isStart[pins] > delay: '- If the difference between the current time and the start time is greater than the delay, increment the cycle counter'
+#            test1 += 1
+#            else:
+#            'do nothing' '- If the difference is less than the delay, do nothing'
+#        elif inputStates[pins] == HIGH & isLast[pins] == LOW: 'If the current state is HIGH and the last state was LOW, reset the last state to HIGH'
+#        isLast[pins] = HIGH 
+#        elif inputStates[pins] == HIGH & isLAst[pins] == HIGH: 'If the current state is HIGH and the last state was HIGH, do nothing'
+#        'do nothing'
 
-def testAssign(chan, cycle_time, duty_cycle, actIn): 'It may make sense to initiate a class with all of the test parameters here'
-    ''' This function assigns the test parameters to the proper test stations.
-        Parameters can include:
-            - Relay channel
-            - ADC addresses (for temperature and current sensors)
-            - DAC addresses (if necessary for modulating outputs)
-            - The desired duty cycle
-            - The length of the test
-        Notice that this test station can run two modulating tests simultaneously with 3 on/off tests, so that's pretty baller
-            - Check the GPIO availability to make sure that's true
-    '''
-    channel = chan 
-    fullCycle = cycle_time / duty_cycle #calculate the test time based on duty cycle and actuator cycle time
-    switchConf = actIn
+#def testAssign(chan, cycle_time, duty_cycle, actIn): 'It may make sense to initiate a class with all of the test parameters here'
+#''' This function assigns the test parameters to the proper test stations.
+#        Parameters can include:
+#            - Relay channel
+#            - ADC addresses (for temperature and current sensors)
+#            - DAC addresses (if necessary for modulating outputs)
+#            - The desired duty cycle
+#            - The length of the test
+#        Notice that this test station can run two modulating tests simultaneously with 3 on/off tests, so that's pretty baller
+#            - Check the GPIO availability to make sure that's true
+#    '''
+#channel = chan 
+#fullCycle = cycle_time / duty_cycle #calculate the test time based on duty cycle and actuator cycle time
+#switchConf = actIn
 
-def relayCheck(variables)
-    '''This function will compare the amount of time that's passed since the last relay state change, and compare it against the required duty cycle
-      - If the time is longer than required by duty cycle
-            - Change the relay state
-            - Reset the time counter
-      - If the time is not longer than required by duty cycle
-            - Do nothing
-      - Iterate through all of the on/off test that are currently running
-    '''
+#def relayCheck(variables)
+#    '''This function will compare the amount of time that's passed since the last relay state change, and compare it against the required duty cycle
+#      - If the time is longer than required by duty cycle
+#            - Change the relay state
+#            - Reset the time counter
+#      - If the time is not longer than required by duty cycle
+#            - Do nothing
+#      - Iterate through all of the on/off test that are currently running
+#    '''
 
-def modCheck(variables)
-    ''' This function will check whether the actuator has reached the required position
-    It does this by comparing the DAC output against the ADC input (For actuators with position tracking)
-    If the actuator doesn't have position tracking, we'll have to do something else
-    Store a moving average of the duty cycle of the actuator on random modulating positions
-        - If the duty cycle is too high, introduce a delay in the program
-    If the actuator is in the correct position, check how long it's been there fore
-        - If it's been long enough, set a new random position
-        - If it hasn't do nothing
-    '''
+#def modCheck(variables)
+#    ''' This function will check whether the actuator has reached the required position
+#    It does this by comparing the DAC output against the ADC input (For actuators with position tracking)
+#    If the actuator doesn't have position tracking, we'll have to do something else
+#    Store a moving average of the duty cycle of the actuator on random modulating positions
+#        - If the duty cycle is too high, introduce a delay in the program
+#    If the actuator is in the correct position, check how long it's been there fore
+#        - If it's been long enough, set a new random position
+#        - If it hasn't do nothing
+#    '''
 
-def getADC(variables)
-    '''This function will call the ADC program from the PiPyADC library
-    results from the call will be stored in a couple of variables
-    If a lot of time has passed, then it's time to write the results to the SD card
-    '''
+#def getADC(variables)
+#    '''This function will call the ADC program from the PiPyADC library
+#    results from the call will be stored in a couple of variables
+#    If a lot of time has passed, then it's time to write the results to the SD card
+#    '''
 
 '''
 The following set-up code was taken from the Relay_Module example made by the creators of the RPi relay board by Waveshare
@@ -121,14 +121,14 @@ actIn = ch1Input
 test1 = 0
 
 try:
-	while True:
-		#Start the test by turning on the relay
+    while True:
+    #Start the test by turning on the relay
         GPIO.output(Relay_Ch1,GPIO.HIGH)
         pos = 'HIGH'
-		print("Actuator Opening")
-		time.sleep(0.1)
+        print("Actuator Opening")
+        time.sleep(0.1)
         cycleStart = time.time()
-        for test1 <= 1000:
+        while 1000 > test1:
             currentTime = time.time()
             if currentTime - cycleStart > actTime:
                 if pos == 'HIGH':
