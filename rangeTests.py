4a90cffb6b239f71020c28d5c26629f89d24ae32
pezLyfe <chris.ray.may@gmail.com>
2019-04-15 16:15:08 -0400

    Added temp measurements and fixed current conveersion

--- rangeTests.py
+++ rangeTests.py
@@ -29,7 +29,7 @@ ads.cal_self()
 ######################## Original Code and Function Definitions from the pipyadc library ################################################
 EXT1, EXT2, EXT3, EXT4 = POS_AIN0|NEG_AINCOM, POS_AIN1|NEG_AINCOM, POS_AIN2|NEG_AINCOM, POS_AIN3|NEG_AINCOM
 EXT5, EXT6, EXT7, EXT8 = POS_AIN4|NEG_AINCOM, POS_AIN5|NEG_AINCOM, POS_AIN6|NEG_AINCOM, POS_AIN7|NEG_AINCOM
-CH_SEQUENCE = (EXT1, EXT2)
+CH_SEQUENCE = (EXT1, EXT2, EXT3)
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 dac = DAC8552()
@@ -52,13 +52,22 @@ def rawConvert(position):
     raw = (position * 64000) + minRaw
     return(raw)
 
-    def currentConvert(raw):
+def currentConvert(raw):
     '''
     This is a linearization function for converting position readings to raw digital readings
     Position readings vary from 0 - 100%, and the raw readings vary from 0 to 8,388,608
     '''
-    volts = (raw * dac.digit_per_v)
-    return(volts)
+    volts = float(raw * ads.v_per_digit)
+    current = (volts * 186)
+    return(current)
+
+def tempConvert(raw):
+    '''
+    Convert temperature readings from the J type thermocouple into a readable format
+    '''
+    volts = float(raw * ads.v_per_digit)
+    temp = (volts - 1.25) / 0.005
+    return(temp)
 
 
 def do_measurement():
@@ -69,9 +78,12 @@ def do_measurement():
     '''
     raw_channels = ads.read_sequence(CH_SEQUENCE) #Read the raw integer input on the channels defined in read_sequence
     pos_channels = int(positionConvert(raw_channels[0]))
-    curr = int(currentConvert(raw_channels[1]))
+    curr = raw_channels[1]
+    temp = raw_channels[2]
+    #curr = int(currentConvert(raw_channels[1]))
+    #temp = tempConvert(raw_channels[2])
     print('act Position', pos_channels, time.time())
-    return(pos_channels, curr)
+    return(pos_channels, curr, temp)
 
 def modulate(modChan):
     aOut = int(np.random.randint(0, high = dac.v_ref) * dac.digit_per_v) #Default arguments of none for size, and I for dtype (single value, and int for data type)
@@ -84,41 +96,58 @@ def modulate(modChan):
 act1Pos = (dac.v_ref / dac.v_ref) * 100
 print(dac.v_ref)
 test1 = 0
-modStart = time.time() #Mark the start time for the cycle
+start = time.time() #Mark the start time for the cycle
 dac.write_dac(DAC_A, dac.v_ref)
 print('DAC_A to HIGH')
 pointTime = []
 posReads = []
+currReads = []
 aOutPoints = []
 lastTime = time.time()
-wait = 0.5
+wait = 0.75
 while test1 < 100000:
     read = do_measurement() # Do a single measurement of the actuator position
-    posReads.append(read) # Append the position reading to a list of position readings
+    curr = currentConvert(read[1])
+    temp = tempConvert(read[2])
+    pos = read[0]
+    posReads.append(pos) # Append the position reading to a list of position readings
+    currReads.append(curr)
     pointTime.append(time.time()) # Append the current time to a list of time points
     aOutPoints.append(act1Pos) # Append the setpoint to a list of setpoints
     lastTime = time.time() - start
     if lastTime > 10000:
         df = pd.DataFrame({ 'time' : pointTime,
                     'Positions' : posReads,
+                    'Current' : currReads,
                     'Set Point' : aOutPoints})
         df.to_csv('actData.csv', sep = ',')
         lastTime = time.time()
     else:
         pass
     
-    if read in range(int(act1Pos - 2), int(act1Pos + 2)):
+    if pos in range(int(act1Pos - 2), int(act1Pos + 2)):
         '''
         If the current position reading on the actuator is within 2% of the position setpoint, change the setpoint
         '''
+        time.sleep(1)
         act1Pos = modulate(DAC_A)
         print('Current Cycle Number is ', test1)
+        print('Actuator Current Draw', curr)
+        print('Actuator Temperature ', temp)
         test1 += 1
         wait = 0.75
-        time.sleep(5)
     else:
         wait = wait * 2
-        print('Set Point', int(act1Pos - 2), read, int(act1Pos + 2))
+        read = do_measurement()
+        pos = read[0]
+        posReads.append(pos) # Append the position reading to a list of position readings
+        currReads.append(curr)
+        pointTime.append(time.time()) # Append the current time to a list of time points
+        aOutPoints.append(act1Pos) # Append the setpoint to a list of setpoints
+        currReads.append(curr)
+        print('Set Point', int(act1Pos - 2), pos, int(act1Pos + 2))
+        print('Current Draw ', curr )
+        print('Actuator Temperature ', temp)
         time.sleep(wait)
 df = pd.DataFrame({ 'time' : pointTime,
                     'Positions' : posReads,
