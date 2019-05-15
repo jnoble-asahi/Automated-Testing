921623ae3be714964edbb7eb332fca0c515eb245
pezLyfe <chris.ray.may@gmail.com>
2019-05-15 15:29:13 -0400

    Minor bug fixes

--- rangeTests.py
+++ rangeTests.py
@@ -37,6 +37,7 @@ import numpy as np
 from ADS1256_definitions import * #Configuration file for the ADC settings
 from pipyadc import ADS1256 #Library for interfacing with the ADC via Python
 import adc_dac_config as an
+import dac8552.dac8552 as dac1
 from dac8552.dac8552 import DAC8552, DAC_A, DAC_B, MODE_POWER_DOWN_100K #Library for using the DAC
 import subprocess
 
@@ -45,33 +46,37 @@ minRaw = 22500
 
 
 ads = ADS1256()
+dac = DAC8552()
 ads.cal_self()
+
+DAC_A = dac1.DAC_A
+DAC_B = dac1.DAC_B
 
 CH1_SEQUENCE = (an.INPUTS_ADDRESS[0], an.INPUTS_ADDRESS[2], an.INPUTS_ADDRESS[5] ) #Position, Current, Temperature channels
 CH2_SEQUENCE =  (an.INPUTS_ADDRESS[1], an.INPUTS_ADDRESS[3], an.INPUTS_ADDRESS[6] ) #Position, Current, Temperature channels
 
 ### Setup for the modulating tests ###
 stamp1 = stamp2 = time.time() # Used as a reference for the datalogger
-ap1 = ap2 = 100
+ap1 = ap2 = dac.v_ref * dac.digit_per_v
 pos1 = pos2 = [] # Create empty lists to add position readings to
 cur1 = cur2 = [] # Create empty lists to add current readings to
 temp1 = temp2 = [] # Create empty lists to add temperature readings to
 ct1 = ct2 = [] # Create empty lists to add timestamps to
 a1 = a2 = [] # Create empty lists to add test setpoints to
-t1 = t2 = [] # Create empty lists to add cycle counts to
+t1 = t2 = 0 # Create empty lists to add cycle counts to
 w1 = w2 = 0.75 # initialize a wait time to reach the next setpoint
 slack1 = slack2 = 2
 
-dac.write_dac(DAC_A, dac.v_ref)
+dac.write_dac(DAC_A, dac.v_ref * dac.digit_per_v)
 print('DAC_A to HIGH')
 
-dac.write_dac(DAC_B, dac.v_ref)
+dac.write_dac(DAC_B, dac.v_ref * dac.digit_per_v)
 print('DAC_B to HIGH')
 t1State = t2State = True
 
-while (t1State | t2State) == True: # If either t1 or t2 still have cycles left, continue the test
+while True: # If either t1 or t2 still have cycles left, continue the test
     if t1 < 1000000:
-        read = an.do_measurement(CH1_SEQUENCE) # Measure a sequence of inputs outline in CH1_Sequence
+        read = an.do_measurement(CH1_SEQUENCE, 0) # Measure a sequence of inputs outline in CH1_Sequence
         pos1.append(read[0])
         cur1.append(read[1])
         temp1.append(read[2])
@@ -87,29 +92,29 @@ while (t1State | t2State) == True: # If either t1 or t2 still have cycles left, 
             stamp1 = time.time()
         else:
             pass
-        if read[0] in range(int(a1 - slack1), int(a1 + slack1)):
+        if read[0] in range(int(ap1 - slack1), int(ap1 + slack1)):
             '''
             If the current position reading on the actuator is within 2% of the position setpoint, change the setpoint
             '''
             time.sleep(1)
             ap1 = an.modulate(DAC_A)
             print('Current Cycle Number is ', t1)
-            print('Actuator Current Draw', cur1)
-            print('Actuator Temperature ', temp1)
+            print('Actuator Current Draw', read[1])
+            print('Actuator Temperature ', read[2])
             t1 += 1
             w1 = 0.75
             slack1 = 2
         else:
             w1 = w1 * 1.5
             slack1 = slack1*1.05
-            print('Set Point', int(a1 - slack1), read[0], int(a1 + slack1))
-            print('Current Draw ', cur1 )
-            print('Actuator Temperature ', temp1)
+            print('Set Point', int(ap1 - slack1), read[0], int(ap1 + slack1))
+            print('Current Draw ', read[1] )
+            print('Actuator Temperature ', read[2])
             time.sleep(w1)
     else:
         t1State = False
     if t2 < 1000000:
-        read = an.do_measurement(CH2_SEQUENCE) # Measure a sequence of inputs outline in CH1_Sequence
+        read = an.do_measurement(CH2_SEQUENCE, 1) # Measure a sequence of inputs outline in CH1_Sequence
         pos2.append(read[0])
         cur2.append(read[1])
         temp2.append(read[2])
@@ -125,24 +130,24 @@ while (t1State | t2State) == True: # If either t1 or t2 still have cycles left, 
             stamp2 = time.time()
         else:
             pass
-        if read[0] in range(int(a2 - slack2), int(a2 + slack2)):
+        if read[0] in range(int(ap2 - slack2), int(ap2 + slack2)):
             '''
             If the current position reading on the actuator is within 2% of the position setpoint, change the setpoint
             '''
             time.sleep(1)
             ap1 = an.modulate(DAC_B)
             print('Current Cycle Number is ', t2)
-            print('Actuator Current Draw', cur2)
-            print('Actuator Temperature ', temp2)
+            print('Actuator Current Draw', read[1])
+            print('Actuator Temperature ', read[2])
             t2 += 1
             w2 = 0.75
             slack2 = 2
         else:
             w2 = w2 * 1.5
             slack2 = slack2*1.05
-            print('Set Point', int(a2 - slack2), read[0], int(a2 + slack2))
-            print('Current Draw ', cur2 )
-            print('Actuator Temperature ', temp2)
+            print('Set Point', int(ap2 - slack2), read[0], int(ap2 + slack2))
+            print('Current Draw ', read[1] )
+            print('Actuator Temperature ', read[2])
             time.sleep(w2)
     else:
         t2State = False
