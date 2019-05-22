6fb73840b69bb84478926f985a01519e2f0aa7d6
pezLyfe <chris.ray.may@gmail.com>
2019-05-22 15:09:16 -0400

    minor tweaks

--- tempMeasurements.py
+++ tempMeasurements.py
@@ -52,19 +52,21 @@ stamp = []
 sampleRate = 30
 writeRate = 1800
 testTime = 21600
-time = time.time()
+start = time.time()
 last = 0
 lastWrite = 0
 
-while (time.time() - time) > testTime:
+while (time.time() - start) < testTime:
     if (time.time() - last) > sampleRate:
-        read1 = an.do_measurement(CH1_SEQ, 0)
+        read1 = an.curTempRead(CH1_SEQ)
         temp1 = temp1.append(an.tempConvert(read1[0]))
         temp2 = temp2.append(an.tempConvert(read1[1]))
         temp3 = temp3.append(an.tempConvert(read1[2]))
         cur1 = cur1.append(an.currentConvert(read1[3]))
         cur2 = cur2.append(an.currentConvert(read1[4]))
         cur3 = cur3.append(an.currentConvert(read1[5]))
+        print('Temp1 Measurement', an.tempConvert(read1[0]))
+        print('Current 1 Measurement', an.currentConvert(read1[4]))
         stamp = stamp.append(time.time())
         last = time.time()
     else:
