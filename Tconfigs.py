997a0766c65a5dbe5849d6e41d60c3b08bebc815
Julia <jnoble@asahi-america.com>
2019-11-21 16:19:41 -0500

    fc

--- Tconfigs.py
+++ Tconfigs.py
@@ -66,7 +66,7 @@ INPUT = binary['INPUT']
 LOW = binary['LOW']
 HIGH = binary['HIGH']
 
-dac.write_dac(DAC_A, an.dac.v_ref) # set brake
+#dac.write_dac(DAC_A, an.dac.v_ref) # set brake
 
 def warning_on():
     red.off() # off function because LED is wired NO
