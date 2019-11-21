43e7106cd9fb5100b38459fb46eeee79a3e5867c
Julia <jnoble@asahi-america.com>
2019-11-21 10:07:05 -0500

    3/5

--- adc_dac_config.py
+++ adc_dac_config.py
@@ -53,11 +53,11 @@ ads.cal_self()
 
 dac = DAC8552()
 print('setting dac')
-dac.v_ref = 3
+dac.v_ref = 5
 step = dac.digit_per_v
 print(dac.v_ref)
 print(step)
-dac.write_dac(DAC_A, 1*step)
+dac.write_dac(DAC_A, 3*step)
 
 ######################## Original Code and Function Definitions from the pipyadc library ################################################
 EXT1, EXT2, EXT3, EXT4 = POS_AIN0|NEG_AINCOM, POS_AIN1|NEG_AINCOM, POS_AIN2|NEG_AINCOM, POS_AIN3|NEG_AINCOM
