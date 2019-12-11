af6576b1e5e1fd70c437102c868a554fc7765a00
Julia <jnoble@asahi-america.com>
2019-12-11 15:50:28 -0500

    dac

--- Measurement-verification.py
+++ Measurement-verification.py
@@ -236,7 +236,7 @@ while True:
         test.append(gcpc.define_test()) # Creates a new gcp test class
         test[i-nos].create_on_off_test() # Loads the test parameters
         print('taking initial measurement at 0')
-        torqueMeasurement(test_channels[0], 4)
+        torqueMeasurement(DAC_A, 4)
         test[i-nos].parameter_check() # Checks that the parameters are within normal working ranges
         tcf.set_on_off(test[i-nos], (i + nos)) # Sets up the IO pins to work for torque tests
         tcf.brakeOn(test[i-nos], (i-nos)) # Turn brake on to setpoint value
