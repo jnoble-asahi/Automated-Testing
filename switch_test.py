e952e19a86f5ec2c36fadc074343cd9e2ed49370
Chris May <chris.ray.may@gmail.com>
2020-01-09 16:13:57 -0500

    Update switch_test.py

--- switch_test.py
+++ switch_test.py
@@ -9,9 +9,9 @@ bash = "sudo pigpiod"
 process = subprocess.Popen(bash.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
 
-ch1 = Button(6)
-ch2 = Button(13)
-ch3 = Button(19)
+ch1 = Button(6, pull_up = True)
+ch2 = Button(13, pull_up = True)
+ch3 = Button(19, pull_up = True)
 
 inputs = (ch1, ch2, ch3)
 
