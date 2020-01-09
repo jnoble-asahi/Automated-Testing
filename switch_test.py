9a5b46e774820db20d99c6b4a3c7a9058bcb691d
pezLyfe <chris.ray.may@gmail.com>
2020-01-09 14:38:42 -0500

    bug fixes

--- switch_test.py
+++ switch_test.py
@@ -11,20 +11,25 @@ output, error = process.communicate()
 
 wp.wiringPiSetupPhys()
 
+binary = {'INPUT' : 0,
+          'OUTPUT': 1,
+          'LOW' : 0,
+          'HIGH' : 1}
+
 ch1 = 31
 ch2 = 33
 ch3 = 35
 
 inputs = (ch1, ch2, ch3)
 
-for item in inputs:
-    wp.pinMode(inputs[item], INPUT) # Declare the pins connected to limit switches as digital inputs
-    wp.pullUpDnControl(inputs[item], 2) # Set the input pins for pull up control
-    print("Pin {} set up for pull up control").format(inputs[item])
+for i, value in enumerate(inputs):
+    wp.pinMode(inputs[i], binary['INPUT']) # Declare the pins connected to limit switches as digital inputs
+    #wp.pullUpDnControl(inputs[i], 2) # Set the input pins for pull up control
+    print("Pin {} set up for pull up control".format(inputs[i]))
 
 
 def switchCheck(channel):
-        state = wp.digitalread(channel)
+        state = wp.digitalRead(channel)
         print(state)
 
 
@@ -34,9 +39,11 @@ while True:
     if time.time() - last < wait:
         pass
     else:
-        for item in inputs:
-            switchCheck(inputs[item])
-            last = time.time()
+        switchCheck(35)
+        last = time.time()
+        #for i, value in enumerate(inputs):
+        #    switchCheck(inputs[i])
+            
     
 
 
