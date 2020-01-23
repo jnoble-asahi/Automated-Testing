b6af533ce3f0a710f680e91ec4bd556e365e7c23
Julia <jnoble@asahi-america.com>
2020-01-23 10:44:45 -0500

    1

--- Tconfigs.py
+++ Tconfigs.py
@@ -121,7 +121,7 @@ def convertSetPoint(test):
     mA = 8.6652e-11*ftlbs**5 - 1.1637e-7*ftlbs**4 + 5.9406e-5*ftlbs**3 - 0.013952*ftlbs**2 + 1.9321*ftlbs + 46.644 # mA needed for brake
     tenV = mA/test.gain # 0-10vdc signal
     fiveV = tenV/2.0 # 0-5vdc signal
-    print ('Brake setpoint:', test.control, 'in-lbs')
+    print ('Brake setpoint:', test.control, 'in-lbs, ', fiveV, "V/5V")
     return fiveV
 
 def torqueMeasurement(input):
