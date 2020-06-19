47a1b4a4fa2a7d69f7705b432a1758a7f6aee2aa
Julia <jnoble@asahi-america.com>
2020-06-19 15:46:56 -0400

    Update digitalRead_test.py

--- digitalRead_test.py
+++ digitalRead_test.py
@@ -10,9 +10,9 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     print ('input 6:', GPIO.input(6))
-    print ('input 19:', GPIO.input(19))
+    print ('input 19:', GPIO.input(26))
     time.sleep(2)
