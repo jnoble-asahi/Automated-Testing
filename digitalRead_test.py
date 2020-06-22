365d133129c43b25158a096dfa6e12e8f25cfea9
Julia <jnoble@asahi-america.com>
2020-06-22 13:45:32 -0400

    gpio

--- digitalRead_test.py
+++ digitalRead_test.py
@@ -5,7 +5,7 @@ import math as mt
 import numpy as np
 import subprocess
 
-import wiringpi as wp
+'''import wiringpi as wp
 
 wp.wiringPiSetupPhys()
 
@@ -14,25 +14,25 @@ s = wp.digitalRead(37)
 print(s)
 time.sleep(3)
 state = wp.digitalRead(7)
-print(state)
+print(state)'''
 
-'''import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-'GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_UP)'
-'GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP)'
-'GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP)'
-GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+'''GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_UP)'''
+GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+'''GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)'''
 
 for x in range (0, 30):
-    print ('input 6:', GPIO.input(31))
+    '''print ('input 6:', GPIO.input(31))'''
     print ('input 13:', GPIO.input(33))
     print ('input 19:', GPIO.input(35))
     print ('input 26:', GPIO.input(37))
     time.sleep(2.5)
 
-GPIO.cleanup()'''
+GPIO.cleanup()
