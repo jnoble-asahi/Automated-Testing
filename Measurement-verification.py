4f20fe88d44e349e537d8e05a5afeacea40459d2
Julia <jnoble@asahi-america.com>
2020-07-01 13:15:09 -0400

    changed pins used

--- Measurement-verification.py
+++ Measurement-verification.py
@@ -52,11 +52,11 @@ INPUTS_ADDRESS = (EXT1, EXT2, EXT3, EXT4, EXT5, EXT6, EXT7, EXT8)
 CH1_Loc = {'cntrl' : DAC_A,
            'torq' : INPUTS_ADDRESS[4],
            'FK_On': 6,
-           'FK_Off' : 13} #GPIO pin numbers
+           'FK_Off' : 19} #GPIO pin numbers
 
 CH2_Loc = {'cntrl' : DAC_B,
            'torq' : INPUTS_ADDRESS[3],
-           'FK_On': 19,
+           'FK_On': 13,
            'FK_Off' : 26} #GPIO pin numbers
 
 CH_Out = {'1' : DAC_A ,
